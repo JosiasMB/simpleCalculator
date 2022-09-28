7cb1748b72d7f8fe8fd5008fac0000bc313cab99
JosiasMB <securejmb@gmail.com>
2022-09-27 20:00:05 -0400

    complete task

--- Calculadora.java
+++ Calculadora.java
@@ -5,41 +5,41 @@ public class Calculadora {
     }
 
     public int sumar(int a, int b, int c){
-        return a + b;
+        return a + b + c;
     }
 
     public int sumar(int a, int b, int c, int d){
-        return a + b;
+        return a + b + c + d;
     }
 
     public int restar(int a, int b){
         return a - b;
     }
     public int restar(int a, int b, int c){
-        return a - b;
+        return a - b - c;
     }
     public int restar(int a, int b, int c, int d){
-        return a - b;
+        return a - b - c - d;
     }
 
     public int multiplicar(int a, int b){
         return a * b;
     }
     public int multiplicar(int a, int b, int c){
-        return a * b;
+        return a * b * c;
     }
     public int multiplicar(int a, int b, int c, int d){
-        return a * b;
+        return a * b * c * d;
     }
 
-    public int dividir (int a, int b){
+    public double dividir (double a, double b){
         return a / b;
     }
-    public int dividir (int a, int b, int c){
-        return a / b;
+    public double dividir (double a, double b, double c){
+        return a / b / c;
     }
-    public int dividir (int a, int b, int c, int d){
-        return a / b;
+    public double dividir (double a, double b, double c, double d){
+        return a / b / c / d;
     }
 }
 
--- testCalculadora.java
+++ testCalculadora.java
@@ -2,8 +2,21 @@ public class testCalculadora {
     public static void main(String[] args) {
         Calculadora cal = new Calculadora();
 
+        System.out.println("--------------- Sumas --------------------");
         System.out.println(cal.sumar(10, 15));
         System.out.println(cal.sumar(10, 15, 5));
         System.out.println(cal.sumar(10, 15, 5, 85));
+        System.out.println("--------------- Restas --------------------");
+        System.out.println(cal.restar(10, 15));
+        System.out.println(cal.restar(10, 15, 5));
+        System.out.println(cal.restar(10, 15, 5, 85));
+        System.out.println("--------------- multiplicaciones --------------------");
+        System.out.println(cal.multiplicar(10, 15));
+        System.out.println(cal.multiplicar(10, 15, 5));
+        System.out.println(cal.multiplicar(10, 15, 5, 85));
+        System.out.println("--------------- Diviciones  --------------------");
+        System.out.println(cal.dividir(10, 15));
+        System.out.println(cal.dividir(10, 15, 5));
+        System.out.println(cal.dividir(10, 15, 5, 85));
     }
 }
